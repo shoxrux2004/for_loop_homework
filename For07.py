9ecdcc2888795accea8511e8c9d72007b51e5e49
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-08-04 06:34:45 +0500

    a dan b gacha toq sum

--- For07.py
+++ For07.py
@@ -6,4 +6,10 @@ def main(N):
     Returns:
         int: return  answer
     """
-    return
+    sum=0
+    i=0
+    for i in range(0,N,2):
+        i+=1
+        sum+=i
+    return sum
+print(main(12))
